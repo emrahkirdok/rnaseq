917ff1f9d15886c7ee4a2a9fcd1685378a2e5ae4
Emrah Kırdök <emrahkirdok@gmail.com>
2024-01-17 15:52:26 +0100

    strand problem fixed

--- scripts/htseq.sh
+++ scripts/htseq.sh
@@ -7,4 +7,4 @@ TRIMMING_TOOL=$2
 ALIGNMENT_TOOL=$3
 COUNT_TOOL=$4
 
-htseq-count results/alignment/${ALIGNMENT_TOOL}/${TRIMMING_TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -n 8 -t gene > results/counts/${COUNT_TOOL}/${ALIGNMENT_TOOL}/${TRIMMING_TOOL}/counts-${SRR}.txt
+htseq-count results/alignment/${ALIGNMENT_TOOL}/${TRIMMING_TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -s no -n 8 -t gene > results/counts/${COUNT_TOOL}/${ALIGNMENT_TOOL}/${TRIMMING_TOOL}/counts-${SRR}.txt
